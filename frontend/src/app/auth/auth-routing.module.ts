f4ca80d4a01d6c019c6d7003865130dd0ea6ace5
username <wecp-dummy@gmail.com>
2025-09-22 19:07:27 +0000

    25th day auth thing

--- frontend/src/app/auth/auth-routing.module.ts
+++ frontend/src/app/auth/auth-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { BankRoutingModule } from "../bank/bank-routing.module";
+import { DashboardComponent } from "../bank/components/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "auth", pathMatch:"full" },
+  {path: "auth", component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AuthRoutingModule {}
+export class AuthRoutingModule { }
